4516ca02663ccbad881d28364615b8ad372450f6
Stephen Whittle <swhittle@crypticwarrior.net>
2021-08-14 17:50:07 +1000

    Add support for specifying the base path rather than assuming working directory

--- source/glob.cpp
+++ source/glob.cpp
@@ -344,9 +344,23 @@ std::vector<fs::path> glob(const std::string &pathname) {
   return glob(pathname, false);
 }
 
+std::vector<std::filesystem::path> glob_path(const std::string& basepath, const std::string& pathname) 
+{
+  return glob(fs::path(basepath) / pathname, false);
+}
+
+
+
 std::vector<fs::path> rglob(const std::string &pathname) {
   return glob(pathname, true);
 }
+
+std::vector<std::filesystem::path> rglob_path(const std::string& basepath, const std::string& pathname) 
+{
+  return glob(fs::path(basepath) / pathname, true);
+}
+
+
 
 std::vector<std::filesystem::path> glob(const std::vector<std::string> &pathnames) {
   std::vector<std::filesystem::path> result;
@@ -358,6 +372,20 @@ std::vector<std::filesystem::path> glob(const std::vector<std::string> &pathname
   return result;
 }
 
+std::vector<std::filesystem::path> glob_path(const std::string& basepath, const std::vector<std::string>& pathnames) 
+{
+  std::vector<std::filesystem::path> result;
+  for (auto& pathname : pathnames)
+  {
+	for (auto& match : glob(fs::path(basepath) / pathname, false))
+	{
+	  result.push_back(std::move(match));
+	}
+  }
+  return result;
+}
+
+
 std::vector<std::filesystem::path> rglob(const std::vector<std::string> &pathnames) {
   std::vector<std::filesystem::path> result;
   for (auto &pathname : pathnames) {
@@ -367,15 +395,35 @@ std::vector<std::filesystem::path> rglob(const std::vector<std::string> &pathnam
   }
   return result;
 }
+std::vector<std::filesystem::path> rglob_path(const std::string& basepath, const std::vector<std::string>& pathnames) 
+{
+  std::vector<std::filesystem::path> result;
+  for (auto &pathname : pathnames) {
+    for (auto &match : glob(fs::path(basepath) / pathname, true)) {
+      result.push_back(std::move(match));
+    }
+  }
+  return result;
+}
+
 
 std::vector<std::filesystem::path>
 glob(const std::initializer_list<std::string> &pathnames) {
   return glob(std::vector<std::string>(pathnames));
 }
 
+std::vector<std::filesystem::path> glob_path(const std::string& basepath,
+											 const std::initializer_list<std::string>& pathnames)
+{
+    return glob_path(basepath, std::vector<std::string>(pathnames));
+}
 std::vector<std::filesystem::path>
 rglob(const std::initializer_list<std::string> &pathnames) {
   return rglob(std::vector<std::string>(pathnames));
 }
-
+std::vector<std::filesystem::path> rglob_path(const std::string& basepath,
+											  const std::initializer_list<std::string>& pathnames)
+{
+    return rglob_path(basepath, std::vector<std::string>(pathnames));
+}
 } // namespace glob
--- standalone/source/main.cpp
+++ standalone/source/main.cpp
@@ -10,38 +10,64 @@ int main(int argc, char** argv) {
 
   bool recursive;
   std::vector<std::string> patterns;
-
+  std::string basepath;
   // clang-format off
   options.add_options()
     ("h,help", "Show help")
     ("v,version", "Print the current version number")
     ("r,recursive", "Run glob recursively", cxxopts::value<bool>(recursive)->default_value("false"))
     ("i,input", "Patterns to match", cxxopts::value<std::vector<std::string>>(patterns))
+    ("b,basepath", "Base directory to glob in", cxxopts::value<std::string>(basepath))
   ;
   // clang-format on
 
   auto result = options.parse(argc, argv);
 
   if (result["help"].as<bool>()) {
+
     std::cout << options.help() << std::endl;
     return 0;
   } else if (result["version"].as<bool>()) {
+
     std::cout << "glob, version " << GLOB_VERSION << std::endl;
     return 0;
   }
 
   if (patterns.empty()) {
+
     std::cout << options.help() << std::endl;
     return 0;
   }
 
   if (recursive) {
-    for (auto& match: glob::rglob(patterns)) {
-     std::cout << match << "\n";
-    } 
+
+    if (!basepath.empty()) {
+
+      for (auto& match : glob::rglob_path(basepath, patterns)) {
+
+        std::cout << match << "\n";
+      }
+    } else {
+
+      for (auto& match : glob::rglob(patterns)) {
+
+        std::cout << match << "\n";
+      }
+    }
   } else {
-    for (auto& match: glob::glob(patterns)) {
-      std::cout << match << "\n";
+
+    if (!basepath.empty()) {
+
+      for (auto& match : glob::glob_path(basepath, patterns)) {
+
+        std::cout << match << "\n";
+      }
+    } else {
+
+      for (auto& match : glob::glob(patterns)) {
+
+        std::cout << match << "\n";
+      }
     }
   }
 
